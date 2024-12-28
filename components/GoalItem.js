5c05297103698a987e58784232ba6e96d501ad51
Nghia <nghiavu144@gmail.com>
2024-12-28 08:14:53 -0600

    Ripple effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,11 +3,17 @@ import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 const GoalItem = ( props ) => { // ({props}) : mean u destructuring and the props 'must' match . 
     return (
-        <Pressable onPress={props.onDelete.bind(this, props.id)}>
-            <View style={styles.goalItem}> 
+        <View style={styles.goalItem}>
+            <Pressable 
+                onPress={props.onDelete.bind(this, props.id)} 
+                android_ripple={{color: '#210644'}}
+                style = {({pressed}) => ( // a fcn will be called automatically whenever the pressed state is changed.
+                    pressed && styles.pressedItem
+                )}
+            > 
                 <Text style= {styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>    
     )
 }
 
@@ -16,11 +22,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8, // Space between elements
-        padding: 8, // Inner space within box
         borderRadius: 6,
         backgroundColor: '#5e0acc',
       },
     goalText : {
+        padding: 8, // Inner space within box
         color: 'white',
+    },
+    pressedItem: {
+        opacity: 0.5,
     }
 })
